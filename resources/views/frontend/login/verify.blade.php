b3537c27fd2f45cc4970bff7a90a672801505aa5
Emerson Valenzuela <144744129+SystemsGV@users.noreply.github.com>
2024-09-18 13:32:16 -0500

    :bug: logo verify

--- resources/views/frontend/login/verify.blade.php
+++ resources/views/frontend/login/verify.blade.php
@@ -172,7 +172,7 @@
             <div class="d-flex d-lg-none justify-content-center pt-4 px-3 mx-auto" style="max-width: 420px;">
                 <a class="navbar-brand text-dark p-0 m-0" href="{{ url('/') }}">
                     <span class="text-primary flex-shrink-0 me-2">
-                        <img src="{{ url('frontend/logo.svg') }}" height="35" alt="">
+                        <img src="{{ url('frontend/logo.svg') }}" height="32%" alt="">
                     </span>
                     Cosmic Bowling
                 </a>
@@ -289,12 +289,8 @@
                 <div class="w-100 d-none d-lg-block" style="max-width: 420px;">
                     <a class="navbar-brand text-dark p-0 m-0" href="index.html">
                         <span class="text-primary flex-shrink-0 me-2">
-                            <svg width="35" height="32" viewbox="0 0 36 33"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill="currentColor"
-                                    d="M35.6,29c-1.1,3.4-5.4,4.4-7.9,1.9c-2.3-2.2-6.1-3.7-9.4-3.7c-3.1,0-7.5,1.8-10,4.1c-2.2,2-5.8,1.5-7.3-1.1c-1-1.8-1.2-4.1,0-6.2l0.6-1.1l0,0c0.6-0.7,4.4-5.2,12.5-5.7c0.5,1.8,2,3.1,3.9,3.1c2.2,0,4.1-1.9,4.1-4.2s-1.8-4.2-4.1-4.2c-2,0-3.6,1.4-4,3.3H7.7c-0.8,0-1.3-0.9-0.9-1.6l5.6-9.8c2.5-4.5,8.8-4.5,11.3,0L35.1,24C36,25.7,36.1,27.5,35.6,29z">
-                                </path>
-                            </svg>
+                            <img src="{{ url('frontend/logo.svg') }}" height="32%" alt="">
+
                         </span>
                         Cosmic Bowling
                     </a>
@@ -310,24 +306,6 @@
                         <a href="{{ route('client.login') }}" class="btn btn-primary mx-2">Iniciar Sesión</a>
                         <a href="{{ route('home.index') }}" class="btn btn-secondary mx-2">Ir a la Página
                             Principal</a>
-                    </div>
-                </div>
-
-                <!-- Social links -->
-                <div class="w-100 mt-auto pb-5" style="max-width: 420px;">
-                    <div class="d-flex justify-content-center justify-content-lg-start gap-3">
-                        <a class="btn btn-icon btn-sm btn-secondary btn-facebook rounded-circle" href="#"
-                            aria-label="Facebook">
-                            <i class="ai-facebook"></i>
-                        </a>
-                        <a class="btn btn-icon btn-sm btn-secondary btn-instagram rounded-circle" href="#"
-                            aria-label="Instagram">
-                            <i class="ai-instagram"></i>
-                        </a>
-                        <a class="btn btn-icon btn-sm btn-secondary btn-linkedin rounded-circle" href="#"
-                            aria-label="LinkedIn">
-                            <i class="ai-linkedin"></i>
-                        </a>
                     </div>
                 </div>
             </div>
